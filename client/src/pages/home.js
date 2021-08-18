f3f35051e3f1c57ae95fec9f5ebbf2d791197747
Suparn Kumar <Sk8969150057@gmail.com>
2021-08-18 17:05:58 +0530

    scroll

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Status from '../components/home/Status'
 import Posts from '../components/home/Posts'
 import { useSelector } from 'react-redux'
 import LoadIcon from '../images/loading.gif'
 import RightSideBar from '../components/home/RightSideBar'
 
+let scroll = 0;
+
 const Home = () => {
     const {postReducer} = useSelector((state) => state)
+
+    window.addEventListener('scroll', () => {
+        if(window.location.pathname === '/'){
+            scroll = window.pageYOffset
+            return scroll
+        }
+    })
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo({top: scroll, behavior: 'smooth'})
+        }, 100)
+    },[])
+
+
     return (
         <div className="home row mx-0">
             <div className="col-md-8">
